014d011461c5c6cebbd00e5fa9918e947b1529ec
Rafael Moreira <rafamsilva@gmail.com>
2025-03-11 10:59:42 +1100

    .develop Created Task Model

---
+++ composeApp/src/commonMain/kotlin/com/feeltheboard/forgo/data/model/Task.kt
@@ -0,0 +1,13 @@
+package com.feeltheboard.forgo.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    @ColumnInfo(name = "title")
+    val title: String
+)
