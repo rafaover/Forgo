bd82c3d45ca45d27d0b9d98245b4d15756326eb1
Rafael Moreira <rafamsilva@gmail.com>
2025-03-12 10:39:53 +1100

    .develop Repository creation

---
+++ composeApp/src/commonMain/kotlin/com/feeltheboard/forgo/data/repository/ForgoRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package com.feeltheboard.forgo.data.repository
+
+import com.feeltheboard.forgo.data.TaskDao
+import com.feeltheboard.forgo.domain.model.Task
+import kotlinx.coroutines.flow.Flow
+
+class ForgoRepositoryImpl(
+    private val taskDao: TaskDao
+) : ForgoRepository {
+    override suspend fun insertTask(task: Task) {
+        taskDao.insertTask(task)
+    }
+
+    override suspend fun deleteTask(task: Task) {
+        taskDao.deleteTask(task)
+    }
+
+    override suspend fun updateTask(task: Task) {
+        taskDao.updateTask(task)
+    }
+
+    override fun getAllTasks(): Flow<List<Task>> {
+        return taskDao.getAllTasks()
+    }
+
+    override suspend fun getTaskByTitle(title: String): Task {
+        return taskDao.getTaskByTitle(title)
+    }
+}
