57f7b337c3f2c92d111a24b04fe910c5b6d6c3c7
Rafael Moreira <rafamsilva@gmail.com>
2025-03-11 11:34:25 +1100

    .develop Created TaskScreen and HomeScreen files.

---
+++ composeApp/src/commonMain/kotlin/com/feeltheboard/forgo/ui/screen/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.feeltheboard.forgo.ui.screen.home
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    // UI elements for displaying the home screen
+    // ...
+}
