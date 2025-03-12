25ae2b32ef2f8c0fbe3733e1e4370fd91c27404f
Rafael Moreira <rafamsilva@gmail.com>
2025-03-12 12:37:30 +1100

    .develop Update Koin Modules and manifest.

--- composeApp/src/commonMain/kotlin/com/feeltheboard/forgo/App.kt
+++ composeApp/src/commonMain/kotlin/com/feeltheboard/forgo/App.kt
@@ -3,11 +3,14 @@ package com.feeltheboard.forgo
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.KoinContext
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        TODO("Call homeScreen here, or navigator")
+        KoinContext {
+            TODO("Introduce composable content here")
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/com/feeltheboard/forgo/di/KoinModules.kt
+++ composeApp/src/commonMain/kotlin/com/feeltheboard/forgo/di/KoinModules.kt
@@ -3,14 +3,17 @@ package com.feeltheboard.forgo.di
 import com.feeltheboard.forgo.data.getRoomDatabase
 import com.feeltheboard.forgo.data.repository.ForgoRepository
 import com.feeltheboard.forgo.data.repository.ForgoRepositoryImpl
+import com.feeltheboard.forgo.ui.screen.home.HomeViewModel
+import com.feeltheboard.forgo.ui.screen.task.TaskViewModel
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 expect val platformModule: Module
 
 val sharedModule = module {
-    single<ForgoRepository> {
-        ForgoRepositoryImpl(taskDao = get())
-    }
+    single<ForgoRepository> { ForgoRepositoryImpl(taskDao = get()) }
     single { getRoomDatabase(get()) }
+    viewModel { HomeViewModel(forgoRepository = get()) }
+    viewModel { TaskViewModel(forgoRepository = get()) }
 }
