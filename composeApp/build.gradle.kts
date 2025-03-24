dbc56559ae2dcf2642deae6cfc1a2a24e64445d8
Rafael Moreira <rafamsilva@gmail.com>
2025-03-24 12:30:38 +1100

    .develop-tip-ops Added testing libs

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -36,6 +36,8 @@ kotlin {
 
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+            implementation(libs.koin.test)
+            implementation(libs.koin.test.junit4)
         }
         
         androidMain.dependencies {
--- composeApp/src/commonTest/kotlin/unit/KointTestingModule.kt
+++ composeApp/src/commonTest/kotlin/unit/KointTestingModule.kt
@@ -1,11 +1,16 @@
 package unit
 
+import com.feeltheboard.forgo.di.appModules
+import org.koin.test.KoinTest
+import org.koin.test.check.checkModules
 import kotlin.test.Test
 
-class KoinTestingModule {
+class KoinModuleTest : KoinTest {
 
     @Test
     fun checkKoinModules() {
-
+        checkModules {
+            modules(appModules())
+        }
     }
 }
